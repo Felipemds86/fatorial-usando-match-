a1f91d30b4fc3f724e98ee567b996bfc183b6afd
Felipe Matheus de Souza <felipemds86@hotmail.com>
2022-09-14 17:01:14 -0300

    Add files via upload

---
+++ fatorial_usando_match.py
@@ -0,0 +1,9 @@
+
+num=int(input("Digite um numero :"))
+while (num !=0):
+    fatorial = contador =0
+    while (contador <= num):
+        fatorial =contador
+        contador =-1  
+    print("O fatorial de" , num , "e:", fatorial)
+    num = int(input("Digite um numero :"))
